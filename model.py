dc33e8c37ecb625a98278b77d3e1568d0437a14f
praneeth <praneethkrishna27@hotmail.com>
2020-04-16 00:31:08 +0530

    changed function name

--- model.py
+++ model.py
@@ -6,19 +6,26 @@ import requests
 
 
 url = []
-def extract_data():
+def generate_url():
+    """
+    this function is used to generate the url's into a list which in further functions is extracted. 
+    """
     for i in range(2012, 2020):
         for j in range(1, 13):
             if j < 10:
                 link = f'https://en.tutiempo.net/climate/0{j}-{i}/ws-432950.html'
                 url.append(link)
-                # print(url)
             else:
                 link = f'https://en.tutiempo.net/climate/{j}-{i}/ws-432950.html'
                 url.append(link)
-                # print(url)
     return url
 
 
-url_values = extract_data()
-print(url_values)
+def extract_data_from_url():
+    
+
+
+
+
+url_values = generate_url()
+extract_data_from_url(url_values)
