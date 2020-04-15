f599f4d28e0731c569f9c7c976236f39137d1f80
praneeth <praneethkrishna27@hotmail.com>
2020-04-16 00:27:05 +0530

    fetch url

--- model.py
+++ model.py
@@ -4,5 +4,21 @@ import os
 from bs4 import BeautifulSoup
 import requests
 
+
+url = []
 def extract_data():
-    pass 
+    for i in range(2012, 2020):
+        for j in range(1, 13):
+            if j < 10:
+                link = f'https://en.tutiempo.net/climate/0{j}-{i}/ws-432950.html'
+                url.append(link)
+                # print(url)
+            else:
+                link = f'https://en.tutiempo.net/climate/{j}-{i}/ws-432950.html'
+                url.append(link)
+                # print(url)
+    return url
+
+
+url_values = extract_data()
+print(url_values)
